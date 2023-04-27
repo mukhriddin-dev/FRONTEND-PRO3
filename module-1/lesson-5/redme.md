e762f7da63d92c219d921ef42bcc3a052718d403
mukhriddin-dev <khodievm@gmail.com>
2023-04-28 00:35:00 +0500

    lesson-4

---
+++ module-1/lesson-5/redme.md
@@ -0,0 +1,28 @@
+- logical operators (&& and || or )
+- Ternary operator
+- "switch" statement
+
+- loops: while and for ( while, do..while and for(..;..;..))
+
+## Function and function types
+
+- Function arrow func / func expression / func declaration
+
+- function parametrs .
+- Default parametr .
+
+- SyntaxError, ReferenceError, TypeError (https://www.tutsmake.com/javascript-error-types/)
+
+
+
+- function declaration 
+
+function func_name(arg1, arg2 , arg3 . . . . . ){
+    // work 
+       clg(arg1)
+}
+
+// run
+
+func_nam()
+
---
+++ module-1/lesson-5/script.js
@@ -0,0 +1,160 @@
+"use strict";
+
+let a = 24
+// --- logical and && , logical or ||
+
+// 150 ball
+
+// IELTS 5 + 5000$
+
+
+// let ball = +prompt('ballni kiriting=')
+// let usd = +prompt('kontraktni kiriting=');
+// let IELTS = +prompt('IELTS darajangini kiriting=');
+
+// //   || -> true
+// //   && -> false
+
+// if ( ball >= 150 ||  IELTS >= 5 && usd >= 5000 ) {
+//     console.log('Congratulations');
+// } else {
+//     console.log("No successfull");
+// }
+
+
+// 3>2 ? console.log('xa') : console.log('yo\'q');
+
+
+// let isBuy=true;
+
+// if(isBuy){
+//     console.log('successfull');
+// }else{
+//     console.log('no successfull');
+// }
+
+
+// isBuy ? console.log('xa') : console.log('yo\'q')
+
+// 4>=3 ? alert('success')  : alert('no successfull');
+
+// let day=4;
+
+// switch(day){
+
+//     case 1 : console.log('Monday'); break;
+//     case 2 : console.log('Tuesday'); break;
+//     case 3 : console.log('Wednesday'); break;
+//     case 4 : console.log('Thursday'); break;
+//     case 5 : console.log('Friday'); break;
+//     case 6 : console.log('Saturday'); break
+//     case 7 : console.log('Sunday'); break;
+
+//     default :  console.log('not found');
+
+// }
+
+// -------- LOOPS --------
+
+
+// WHILE , DO WHILE , FOR
+
+
+// WHILE
+
+// let n = 1;
+
+// while (n <= 100) {
+
+//     if (n % 10 == 0) {
+//         console.log(n)
+//     }
+
+//     n++;
+// }
+
+// DO WHILE
+
+
+// do {
+//     n++;
+//     console.log(n)
+
+// } while (n < 10)
+
+
+// FOR
+
+
+// for (let n = 0 ; n <2; n++) {
+//     console.log(n)
+// }
+
+
+// function decloration
+
+// camel case
+// snake case
+// pascal case
+
+// function myName(){}   // camel case 75%
+// function my_name(){}  // snake case 10%
+// function My_Name(){}  // Pascal case 5%
+
+
+// template literal -> interpolation
+
+// function sayMyName(a,b,c) {
+
+//     console.log('Mukhriddin'+c);
+//     console.log(`${a}ning kasbi ${b}`) //ES6
+
+// }
+
+// sayMyName('Samandar','Programmer', 'web');
+// sayMyName('Bobur','UX designer', 'web');
+// sayMyName('Saidabror','Copyrighter', 'web');
+
+// function counter(a,b){
+//      return 123/a
+// }
+
+// console.log( counter(10) )
+
+
+// FUNCTION EXPRESSION
+
+// const calc = function (a, b, c) {
+   
+//     return a+b+c;
+// }
+
+// console.log( calc('javascript ', ' Vue.js ', ' Nuxt.js') )
+
+
+// const letter=calc;
+// const test=letter;
+// test('javascript','javascript' ,'javascript')
+// letter('A','B', 'C')
+
+// ----- ARROW FUNCTION ------
+
+// ES6 
+
+// const message=(a,b,c)=> a+b+c
+
+// console.log(message('javascript ',' php ',' typescript'))
+
+// const app=message;
+
+// app('javascript','tailwind', 'ux/ui')
+
+// const app=(a='VueJS')=>{
+//     console.log(a)
+// }
+// app('Reactjs')
+
+// return 
+
+
+
