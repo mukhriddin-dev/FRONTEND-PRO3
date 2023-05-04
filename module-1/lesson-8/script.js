7a7001f51d50ea6adc3ba4b14c705dc0d876a8e6
mukhriddin-dev <khodievm@gmail.com>
2023-05-05 00:26:38 +0500

    lesson 9 completed

--- module-1/lesson-8/script.js
+++ module-1/lesson-8/script.js
@@ -1,23 +1,30 @@
 "use strict";
 
-let  text=document.querySelector('.text'),
-     input=document.querySelector('input'),
-     btn=document.querySelector('.btn');
+// const laod=setTimeout(()=>{
+//       location.reload();
+// },1200)
+
+window.addEventListener('DOMContentLoaded', ()=>{
 
 
-// nodeName -> 
+// let  text=document.querySelector('.text'),
+//      input=document.querySelector('input'),
+//      btn=document.querySelector('.btn');
+
+// nodeName ->
 // tagName ->
-// nodeType -> 
+// nodeType ->
 // className ->
 
 // --> getAttribute('nameATTR') -> HAR QANDAY ATTRIBUTE QIYMATINI OLIB BERADI !
 
 // --> setAttribute('nameATTR',value) -> HAR QANDAY ATTRIBUTEGA QIYMAT TAYINLAYDI !  YOKI QO'SHADI !
 
+// --> hasAttribute -> attribute bor yoki yo'qligini tekshiradi .
+
 // --> CSStyleDeclaration -> OBJECT har qanday elementda mavjud bo'sh propertylar obyekti.
 
-// dataset attribute --> 
-
+// dataset attribute -->
 
 // function setInput(){
 //     // input.setAttribute('type', "password");
@@ -25,10 +32,9 @@ let  text=document.querySelector('.text'),
 //     console.log(input.value);
 // }
 
-// // inline - > attributes 
+// // inline - > attributes
 
 // btn.onclick=setInput;
-
 
 // console.log(text.style.backgroundColor="aqua");
 // text.style.color="red";
@@ -37,8 +43,7 @@ let  text=document.querySelector('.text'),
 
 // text.style.cssText="background-color:aqua; color:red; width:500px; margin:30px";
 
-// // onClick , onChange , onIn    
-
+// // onClick , onChange , onIn
 
 // input.oninput=function(){
 //       if(input.value.trim().length>6){
@@ -48,6 +53,133 @@ let  text=document.querySelector('.text'),
 //       }
 // }
 
+// dataset
+
+let photos = document.querySelectorAll(".cover");
+
+// photos[0].dataset.types="photo";
+// photos[1].dataset.types="photo";
+
+// photos[2].dataset.types="art";
+// photos[3].dataset.types="art";
+
+// photos[4].dataset.types="tech";
+// photos[5].dataset.types="tech"
+
+// photos[6].dataset.types="music";
+// photos[7].dataset.types="music";
+
+// diynamic ->
+
+// createElement -> dinamik element genartsiya qiladi.
+// append -> ohiridan element qo'shadi
+// prepend -> boshidan element qo'shadi
+
+let images = [
+  {
+    url: "https://picsum.photos/id/311/300/300",
+    category: "photo",
+    id: 1,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/312/300/300",
+    category: "photo",
+    id: 2,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/313/300/300",
+    category: "photo",
+    id: 3,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/314/300/300",
+    category: "art",
+    id: 4,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/315/300/300",
+    category: "art",
+    id: 5,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/316/300/300",
+    category: "art",
+    id: 10,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/317/300/300",
+    category: "tech",
+    id: 6,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/318/300/300",
+    category: "tech",
+    id: 7,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/319/300/300",
+    category: "tech",
+    id: 8,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/320/300/300",
+    category: "sport",
+    id: 9,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/321/300/300",
+    category: "life",
+    id: 8,
+    title: "this is a photo",
+  },
+  {
+    url: "https://picsum.photos/id/322/300/300",
+    category: "life",
+    id: 9,
+    title: "this is a photo",
+  },
+];
+
+let gallery = document.querySelector(".gallery");
+let select = document.querySelector("#photos");
+
+let options = [];
+
+function addElmant(obj) {
+  let img = document.createElement("img");
+  let option = document.createElement("option");
+
+  if (!options.includes(obj.category)) {
+    options.push(obj.category);
+    select.append(option);
+  }
+
+  option.value = obj.category;
+  option.textContent = obj.category;
+
+  img.setAttribute("src", `${obj.url}`);
+  img.setAttribute("alt", `${obj.title}`);
+  img.dataset.category = obj.category;
+  gallery.append(img);
+}
 
 
 
+
+for (let i = 0; i < images.length; i++) {
+  addElmant(images[i]);
+}
+
+
+
+})
